8abfbd59834905b10c17908715edb1954b5dc339
Darryl Kilzer <darrylkilzer@gmail.com>
2020-02-17 09:52:53 -0700

    start is done

--- YahtzeeScorer/Scorer.cs
+++ YahtzeeScorer/Scorer.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace YahtzeeScorer
 {
     public class Scorer
     {
-        
-
-        
+        public int ScoreByNumber(int numToScoreBy, int[] dice)
+        {
+            int score = 0;
+            if(dice.Length == 5)
+            {
+                for(int i = 0; i < dice.Length; i++)
+                {
+                    int die = dice[i];
+                    if(die == numToScoreBy && die < 7 && die > 0)
+                    {
+                        score += die;
+                    }
+                }
+            }
+            return score;
+        }
     }
 }
--- YahtzeeTests/Tests.cs
+++ YahtzeeTests/Tests.cs
@@ -6,7 +6,54 @@ namespace YahtzeeTests
 {
     public class Tests
     {
-        
+        [Fact]
+        public void Can_Score_30_Given_Five_Sixes()
+        {
+            Scorer sc = new Scorer();
+            int[] dice = new int[] { 6, 6, 6, 6, 6 };
+            int expected = 30;
+            int numToScoreBy = 6;
+            int actual = sc.ScoreByNumber(numToScoreBy, dice);
+            Assert.Equal(expected, actual);
+        }
+  
+
+        [Fact]
+        public void Can_Score_0_Given_Six_Sixes()
+        {
+            Scorer sc = new Scorer();
+            int[] dice = new int[] { 6, 6, 6, 6, 6, 6 };
+            int expected = 0;
+            int numToScoreBy = 6;
+            int actual = sc.ScoreByNumber(numToScoreBy, dice);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Can_Score_24_Given_Four_Sixes()
+        {
+            Scorer sc = new Scorer();
+            int[] dice = new int[] { 6, 6, 6, 6, 5 };
+            int expected = 24;
+            int numToScoreBy = 6;
+            int actual = sc.ScoreByNumber(numToScoreBy, dice);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 4, 2, 2, 1}, 4, 8)]
+        [InlineData(new int[] { 4, 1, 2, 2, 1}, 4, 4)]
+        [InlineData(new int[] { 4, 1, 2, 2, 1}, 1, 2)]
+        [InlineData(new int[] { 4, 4, 2, 2, 2}, 2, 6)]
+        [InlineData(new int[] { 4, 2, 2, 7, 7}, 7, 0)]
+        [InlineData(new int[] { 2, 2, 2, 4, 4}, 2, 6)]
+        [InlineData(new int[] { 4, 2, 2, -7, -7}, -7, 0)]
+        public void Can_Score_Number(int[] dice, int numToScoreBy, int expected)
+        {
+            Scorer sc = new Scorer();
+            int actual = sc.ScoreByNumber(numToScoreBy, dice);
+            Assert.Equal(expected, actual);
+        }
 
     }
 }
